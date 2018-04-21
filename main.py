a109a6ad5341b7d882bb1c4d5c9258c3f1688374
Yehoshua Oliveira <ioxua.oliveira@gmail.com>
2018-04-20 21:17:35 -0300

    fix: adds endpoints to the variour index pages and login

--- main.py
+++ main.py
@@ -12,14 +12,30 @@ app.wsgi_app = SassMiddleware(app.wsgi_app, {
 })
 
 
-@app.route('/', methods=['GET', 'POST'])
-@app.route('/<name>', methods=['GET', 'POST'])
-def hello(name=None):
-	if(request.method == 'GET'):
-		return render_template('website/index.pug', name=name)
+#@app.route('/', methods=['GET', 'POST'])
+#@app.route('/<name>', methods=['GET', 'POST'])
+#def hello(name=None):
+#	if(request.method == 'GET'):
+#		return render_template('website/index.pug', name=name)
 
-	elif(request.method == 'POST'):
-		return render_template('website/hello.pug', name="Você mandou um POST \o/")
+#	elif(request.method == 'POST'):
+#		return render_template('website/hello.pug', name="Você mandou um POST \o/")
+
+@app.route('/index', methods=['GET', 'POST'])
+def index():
+	return render_template('website/index.pug')
+
+@app.route('/index2', methods=['GET', 'POST'])
+def index2():
+return render_template('website/index2.pug')
+
+@app.route('/index3', methods=['GET', 'POST'])
+def index3():
+return render_template('website/index3.pug')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+return render_template('website/login.pug')
 
 @app.errorhandler(404)
 def not_found(error):
