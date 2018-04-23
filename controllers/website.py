c02a052fd8d0f3915ce42f9751d65b6785d8c588
Yehoshua Oliveira <ioxua.oliveira@gmail.com>
2018-04-23 17:16:37 -0300

    chore: implements some test endpoints

--- controllers/website.py
+++ controllers/website.py
@@ -6,13 +6,24 @@ website_controller = Blueprint('website_controller', __name__)
 def index():
 	return render_template('website/index.pug')
 
+
 @website_controller.route('/indexTest', methods=['GET'])
 def index2():
 	return render_template('website/index-test.pug')
 
+
+@website_controller.route('/me', methods=['GET'])
+def me():
+	return render_template('tests/todo.pug', next='http://localhost:5000/secret', message='Faça login primeiro D:')
+
+
+@website_controller.route('/secret', methods=['GET'])
+def secret():
+	return render_template('tests/todo.pug', message='Se você tá vendo isso, você está logado!')
+
 @website_controller.route('/login', methods=['GET', 'POST'])
 def login():
 	if(request.method == 'GET'):
-		return render_template('website/login.pug')
+		return render_template('website-commons/login.pug')
 	else:
 		abort(404)
